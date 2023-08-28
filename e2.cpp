252c147d4691bb7b91e668be9c3831a251628d09
LsBhrqz <luisam.bohorqueza@udea.edu.co>
2023-08-28 15:56:36 -0500

    Create e2.cpp

---
+++ e2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int ma(){
+    int n;
+    cout << "Ingrese N: ";
+    cin >> n;
+    cout << n << "Es ";
+    if (n%2==0){
+        cout << "par \n";
+    }
+    else{
+        cout << "impar \n";
+    }
+    return 0;
+}
