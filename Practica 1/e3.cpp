8d0ce029743dd693c8047213704b1a90310d5661
LsBhrqz <luisam.bohorqueza@udea.edu.co>
2023-08-28 16:54:42 -0500

    Avance de clase

---
+++ Practica 1/e3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B;
+    cout << "Ingrese el valor de A: ";
+    cin >> A;
+    cout << "Ingrese el valor de B: ";
+    cin >> B;
+    if (A>B){
+        cout <<"El valor mayor es "<< A;
+    }else{
+        cout <<"El valor mayor es "<< B;
+    }
+}
