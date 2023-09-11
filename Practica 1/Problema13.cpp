10c236b8594b121e0c28cc9a65535ef576e8cd3b
LsBhrqz <luisam.bohorqueza@udea.edu.co>
2023-09-11 13:35:12 -0500

    Create Problema13.cpp

---
+++ Practica 1/Problema13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+bool esprimo(int numero);
+int main()
+{
+    int numero=0, rfinal=0, val=0;
+    cout<<"Ingrese un numero: "; cin>>numero;
+    for(int i=2; i<numero ;i++){
+        val=esprimo(i);
+        if(val){
+            rfinal+=i;
+        }
+    }
+    cout<<"El resultado de la suma es: "<<rfinal<<endl;
+}
+
+bool esprimo(int numero){
+    if (numero==1)
+        return false;
+    else if (numero==2){
+        return true;
+    }
+    else if(numero%2==0){
+        return false;
+    }
+    for(int i=3; i*i<=numero;i+=2){
+        if(numero%i==0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
